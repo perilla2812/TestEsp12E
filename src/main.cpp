64beba54c028c762a760fd952bfecb404537e132
Rodolfo Tiburcio <fulladdict@gmail.com>
2017-01-10 02:08:08 -0600

    Configuracion final para prueba

--- src/main.cpp
+++ src/main.cpp
@@ -4,8 +4,8 @@
 #include "Arduino.h"
 #include <ESP8266WiFi.h>
 
-const char* ssid = "ARRIS-A222";
-const char* password = "104BCCAF9A08FEC2";
+const char* ssid = "XXXX-XXXXX";
+const char* password = "XXXXXXXXXXXXXX";
 
 const int PIN_LED = 5;
 
