9ca7003a8d0b77b4be0f28815dc78d4e873a4fb9
Rodolfo Tiburcio <fulladdict@gmail.com>
2017-01-10 02:01:01 -0600

    Creación del repositorio
    Creamos el repositorio de GitHub con el programa ya echo

---
+++ src/main.cpp
@@ -0,0 +1,41 @@
+//*****************************************************************************
+//****Programa para probar el ESP12E, realizando la conexión a una red Wifi****
+//*****************************************************************************
+#include "Arduino.h"
+#include <ESP8266WiFi.h>
+
+const char* ssid = "ARRIS-A222";
+const char* password = "104BCCAF9A08FEC2";
+
+const int PIN_LED = 5;
+
+void setup_wifi();
+
+void setup(){
+  pinMode(PIN_LED, OUTPUT);
+  pinMode(BUILTIN_LED, OUTPUT);
+  Serial.begin(115200);
+  setup_wifi();
+}
+
+void setup_wifi(){
+  delay(10);
+  Serial.println();
+  Serial.print("Conectando a ");
+  Serial.println(ssid);
+
+  WiFi.begin(ssid, password);
+
+  while (WiFi.status()!=WL_CONNECTED){
+    delay(500);
+    Serial.print(".");
+  }
+
+  Serial.println("");
+  Serial.println("WiFi conectado");
+  Serial.println("Dirección IP: ");
+  Serial.println(WiFi.localIP());
+}
+
+void loop(){
+}
